dc2aa726a2da1f404c7a42362eb7d06f3eb8c394
Naveen Pantra <naveenpantra.np@gmail.com>
2023-02-15 23:55:00 +0530

    ascii char type

---
+++ algo/000_PSCP_lab/week_03/003_char_type.rs
@@ -0,0 +1,18 @@
+/*
+
+A character is entered through keyboard. Write a program to determine whether the Character
+entered is a capital letter, a small case letter, a digit or a special symbol.
+The following table shows the range of ASCII values for various characters.
+Characters ASCII values Special Symbols
+
+ A-Z                65 - 90
+ a-z                97- 122
+ 0 - 9              48 - 57
+ Special Symbols    0 - 47, 58 - 64, 91- 96, 123 - 127
+
+ */
+
+fn main() {
+    let mut char_string: String = "".to_string();
+    std::io::stdin().
+}
