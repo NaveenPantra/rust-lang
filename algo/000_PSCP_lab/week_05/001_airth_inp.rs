054bc4a3d062db757a72ecd696ca4f2bc5f2fcaa
Naveen Pantra <naveenpantra.np@gmail.com>
2023-04-13 22:09:26 +0530

    Create 001_airth_inp.rs

---
+++ algo/000_PSCP_lab/week_05/001_airth_inp.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let (operand1, operator, operand2): (i32, &str, i32) = match args.as_slice() {
+        [operand1, operator, operand2] => (
+            operand1.parse::<i32>().expect("Operand1 must be an 32bit integer"),
+            operator,
+            operand2.parse::<i32>().expect("Operand1 must be an 32bit integer"),
+        ),
+        _ => panic!("\nPlease give three args seperated by a space. Ex: 1 + 2\n")
+    };
+    // Default wrapping arithmetic
+    let result: i32 = match operator {
+        "+" => operand1 + operand2,
+        "-" => operand1 - operand2,
+        "*" => operand1 * operand2,
+        "/" => operand1 / operand2,
+        "%" => operand1 % operand2,
+        _ => panic!("Please provide a valid arithmetic operator ex: +, -, *, /, %"),
+    };
+    println!("Result of {operand1} {operator} {operand2} = {result}");
+}
